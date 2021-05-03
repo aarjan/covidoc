c49ebc97ce1d9ba5be7347befb6cddc2f298790d
aarjan <aarjan.baskota@gmail.com>
2021-05-03 16:10:49 +0545

    Add support for doctor profile update

--- lib/ui/screens/profile/profile_screen.dart
+++ lib/ui/screens/profile/profile_screen.dart
@@ -1,11 +1,13 @@
-import 'package:covidoc/bloc/bloc.dart';
-import 'package:covidoc/bloc/user/user_bloc.dart';
-import 'package:covidoc/model/entity/app_user.dart';
-import 'package:covidoc/ui/widgets/widgets.dart';
-import 'package:covidoc/utils/const/const.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:covidoc/bloc/bloc.dart';
 import 'package:covidoc/utils/utils.dart';
+import 'package:covidoc/utils/const/const.dart';
+import 'package:covidoc/ui/widgets/widgets.dart';
+import 'package:covidoc/bloc/user/user_bloc.dart';
+import 'package:covidoc/model/entity/app_user.dart';
 
 import 'components.dart';
 
@@ -26,10 +28,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
   String _location;
   String _language;
   String _symptoms;
+  String _practice;
+  String _speciality;
+  String _yearsOfExperience;
 
   AppUser _user;
   int _status = 0;
   FocusScopeNode _node;
+
+  final covidStatus = ['Positive', 'Negative', 'Not Tested'];
+  List<String> years = ['1-2 years', '2-5 years', '5-10 years', '>10 years'];
 
   void init(AppUser user) {
     _user = _user ?? user;
@@ -38,9 +46,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
     _language = _language ?? user.detail['language'];
     _location = _location ?? user.detail['location'];
     _symptoms = _symptoms ?? user.detail['symptoms'];
+    _practice = _practice ?? user.detail['practice'];
+    _speciality = _speciality ?? user.detail['speciality'];
+    _yearsOfExperience =
+        _yearsOfExperience ?? user.detail['_yearsOfExperience'];
   }
 
-  final covidStatus = ['Positive', 'Negative', 'Not Tested'];
   void updateState(int val) {
     setState(() {
       _status = val;
@@ -83,7 +94,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   SliverList(
                     delegate: SliverChildListDelegate([
                       const SizedBox(height: 20),
-                      buildForm(),
+                      _user.type == describeEnum(UserType.Patient)
+                          ? buildPatientForm()
+                          : buildDoctorForm(),
                     ]),
                   ),
                 ],
@@ -92,7 +105,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 const Center(child: CircularProgressIndicator())
             ],
           );
-        },
+          },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -108,6 +121,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   'location': _location,
                   'symptoms': _symptoms,
                   'covidStatus': _status,
+                  'practice': _practice,
+                  'speciality': _speciality,
+                  'yearsOfExperience': _yearsOfExperience,
                 },
                 profileVerification: true,
               );
@@ -128,7 +144,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  Form buildForm() {
+  Form buildPatientForm() {
     return Form(
         key: _formKey,
         child: Padding(
@@ -193,6 +209,92 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 onSave: (val) => _symptoms = val.trim(),
               ),
               const SizedBox(height: 16),
+            ],
+          ),
+        ));
+  }
+
+  Form buildDoctorForm() {
+    return Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              FormInput(
+                title: 'Age',
+                isNumber: true,
+                onEdit: _node.nextFocus,
+                enabled: _enabled,
+                initialValue: _age.toString(),
+                onSave: (val) => _age = int.tryParse(val.trim(), radix: 10),
+                onValidate: (val) =>
+                    val.trim().isEmpty ? AppConst.AGE_ERROR : null,
+              ),
+              const SizedBox(height: 16),
+              FormInput(
+                title: 'Gender',
+                initialValue: _gender,
+                onEdit: _node.nextFocus,
+                enabled: _enabled,
+                onSave: (val) => _gender = val.trim(),
+                onValidate: (val) =>
+                    val.trim().isEmpty ? AppConst.GENDER_ERROR : null,
+              ),
+              const SizedBox(height: 16),
+              FormInput(
+                title: 'Location',
+                initialValue: _location,
+                onEdit: _node.nextFocus,
+                enabled: _enabled,
+                onSave: (val) => _location = val.trim(),
+                onValidate: (val) =>
+                    val.trim().isEmpty ? AppConst.FULLNAME_LENGTH_ERROR : null,
+              ),
+              const SizedBox(height: 16),
+              FormInput(
+                title: 'Speciality',
+                initialValue: _speciality,
+                onEdit: _node.nextFocus,
+                enabled: _enabled,
+                onSave: (val) => _speciality = val.trim(),
+                onValidate: (val) => val.trim().isEmpty
+                    ? AppConst.SPECIALITY_LENGTH_ERROR
+                    : null,
+              ),
+              const SizedBox(height: 16),
+              FormInput(
+                title: 'Hospital or Clinic',
+                enabled: _enabled,
+                initialValue: _practice,
+                onEdit: _node.nextFocus,
+                onSave: (val) => _practice = val.trim(),
+              ),
+              const SizedBox(height: 16),
+              Row(children: [
+                Expanded(
+                    child: Text(
+                  'Years of Experience',
+                  style: AppFonts.MEDIUM_BLACK3_16,
+                )),
+                DropdownButton(
+                  value: _yearsOfExperience ?? years[0],
+                  underline: const SizedBox.shrink(),
+                  items: years
+                      .map((e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ))
+                      .toList(),
+                  onChanged: (val) {
+                    setState(() {
+                      _yearsOfExperience = val;
+                    });
+                  },
+                )
+              ]),
+              const SizedBox(height: 50),
             ],
           ),
         ));
