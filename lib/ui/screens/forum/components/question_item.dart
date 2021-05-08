4475b5b5d36a4bec6f03ca1eff233638ad45242a
aarjan <aarjan.baskota@gmail.com>
2021-05-08 11:44:50 +0545

    Update questionItem with popUpBtn & imageSlider

--- lib/ui/screens/forum/components/question_item.dart
+++ lib/ui/screens/forum/components/question_item.dart
@@ -1,13 +1,18 @@
+import 'package:covidoc/bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:covidoc/utils/utils.dart';
 import 'package:covidoc/utils/const/const.dart';
+import 'package:covidoc/ui/widgets/widgets.dart';
 import 'package:covidoc/model/entity/entity.dart';
 
+import 'add_update_question_modal.dart';
 import 'discussion_count.dart';
+import 'image_slider.dart';
 
 class QuestionItem extends StatelessWidget {
   final Forum question;
@@ -25,27 +30,24 @@ class QuestionItem extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              question.title,
-              maxLines: 2,
-              softWrap: true,
-              textAlign: TextAlign.start,
-              style: AppFonts.REGULAR_BLACK3_14,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  question.title,
+                  maxLines: 2,
+                  softWrap: true,
+                  textAlign: TextAlign.start,
+                  style: AppFonts.REGULAR_BLACK3_14,
+                ),
+                DiscussionPopUpMenu(question: question)
+              ],
             ),
             const SizedBox(
               height: 10,
             ),
             if (question.imageUrls.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: CachedNetworkImage(
-                    imageUrl: question.imageUrls[0],
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
-              ),
+              ImageSlider(images: question.imageUrls),
             Row(
               children: [
                 SvgPicture.asset('assets/forum/calendar.svg'),
@@ -113,6 +115,80 @@ class QuestionItem extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DiscussionPopUpMenu extends StatelessWidget {
+  const DiscussionPopUpMenu({
+    Key key,
+    @required this.question,
+  }) : super(key: key);
+
+  final Forum question;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      onSelected: (str) {
+        switch (str) {
+          case 'delete':
+            context.read<ForumBloc>().add(DeleteForum(question.id));
+            break;
+          case 'edit':
+            showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                backgroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  side: BorderSide.none,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                ),
+                builder: (context) {
+                  return AddUpdateQuestionModal(
+                    question: question,
+                  );
+                });
+            break;
+          case 'report':
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              backgroundColor: Colors.white,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              builder: (context) {
+                return ReportForumModal(
+                  onSubmit: (String report, String reportType) {
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+            break;
+          default:
+        }
+      },
+      icon: const Align(
+        alignment: Alignment.centerRight,
+        child: Icon(Icons.more_vert),
+      ),
+      padding: EdgeInsets.zero,
+      itemBuilder: (context) => [
+        const PopupMenuItem(
+          child: Text('Edit'),
+          value: 'edit',
+        ),
+        const PopupMenuItem(
+          child: Text('Delete'),
+          value: 'delete',
+        ),
+        const PopupMenuItem(
+          child: Text('Report'),
+          value: 'report',
+        ),
+      ],
     );
   }
 }
