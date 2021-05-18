0a8ef110308d81c219c4710749532503eecc0cac
aarjan <aarjan.baskota@gmail.com>
2021-05-18 11:58:33 +0545

    Show progressLoader in foreground

--- lib/ui/screens/forum/forum_screen.dart
+++ lib/ui/screens/forum/forum_screen.dart
@@ -27,6 +27,7 @@ class ForumScreen extends StatefulWidget {
 
 class _ForumScreenState extends State<ForumScreen>
     with TickerProviderStateMixin {
+  List<Forum>? _forums;
   late bool _showAddBtn;
   bool _isLoading = false;
   String _category = 'All';
@@ -37,6 +38,12 @@ class _ForumScreenState extends State<ForumScreen>
   void initState() {
     super.initState();
     _showAddBtn = false;
+
+    // -----------------------------------------------------------------------
+    // SCROLLCONTROLLER IS USED TO SHOW/HIDE FAB ICON & ADDQUESTION CONTAINER
+    // IF SCROLLDIRECTION IS REVERSE (SCROLL DOWN) FAB ICON IS DISPLAYED
+    // IF SCROLLDIRECTION IS FORWARD (SCROLL UP) ADDQUESTION IS DISPLAYED
+    // ------------------------------------------------------------------------
     _scrollController = ScrollController();
     _scrollController.addListener(() {
       if (_scrollController.position.userScrollDirection ==
@@ -54,7 +61,9 @@ class _ForumScreenState extends State<ForumScreen>
         });
       }
 
-      // Fetch more items when reached the bottom of screen
+      // ------------------------------------------------------------------
+      // FETCH MORE ITEMS WHEN REACHED THE BOTTOM OF THE SCREEN
+      // ------------------------------------------------------------------
       final maxExtent = _scrollController.position.maxScrollExtent * 0.9;
       if (_scrollController.position.pixels > maxExtent && !_isLoading) {
         // context.read<ForumBloc>().add(LoadForum(hardRefresh: true));
@@ -78,105 +87,131 @@ class _ForumScreenState extends State<ForumScreen>
         }
       }, builder: (context, state) {
         if (state is ForumLoadSuccess) {
-          return RefreshIndicator(
-            onRefresh: () async {
-              context.read<ForumBloc>().add(LoadForum(hardRefresh: true));
-            },
-            child: SingleChildScrollView(
-                controller: _scrollController,
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                physics: const AlwaysScrollableScrollPhysics(
-                  parent: BouncingScrollPhysics(),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    AnimatedSize(
-                      vsync: this,
-                      curve: Curves.easeIn,
-                      duration: const Duration(milliseconds: 400),
-                      child: Visibility(
-                        visible: !_showAddBtn,
-                        child: AddQuestionView(
-                          onAdd: () {
-                            showAddQuestionModal(
-                                context, widget.isAuthenticated);
-                          },
-                        ),
+          _forums = state.forums;
+        }
+        return Stack(
+          children: [
+            // --------------------------------------------------------
+            // REFRESH INDICATOR FOR PULL TO REFRESH
+            // --------------------------------------------------------
+            RefreshIndicator(
+              onRefresh: () async {
+                context.read<ForumBloc>().add(LoadForum(hardRefresh: true));
+              },
+              child: SingleChildScrollView(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  physics: const AlwaysScrollableScrollPhysics(
+                    parent: BouncingScrollPhysics(),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Flexible(
-                          child: DropdownFilter(
-                            title: 'Category',
-                            fillColor: AppColors.WHITE4,
-                            items: _categories,
-                            selectedCategory: _category,
-                            onItemSelected: (val) {
-                              _category = val;
-                              setState(() {});
-                              context
-                                  .read<ForumBloc>()
-                                  .add(LoadForum(category: val));
+                      // ------------------------------------------------------
+                      // ANIMATEDSIZE WIDGET FOR ANIMATING ADD QUESTION
+                      // VISIBIBLE IF _showAddBtn == false
+                      // ------------------------------------------------------
+                      AnimatedSize(
+                        vsync: this,
+                        curve: Curves.easeIn,
+                        duration: const Duration(milliseconds: 400),
+                        child: Visibility(
+                          visible: !_showAddBtn,
+                          child: AddQuestionView(
+                            onAdd: () {
+                              showAddQuestionModal(
+                                  context, widget.isAuthenticated);
                             },
                           ),
                         ),
-                        const SizedBox(width: 10),
-                        Flexible(
-                          child: DropdownFilter(
-                            title: 'Sort',
-                            fillColor: AppColors.WHITE4,
-                            items: ['Sort1', 'Sort2', 'Sort3'],
-                            onItemSelected: (val) {},
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // ------------------------------------------------------
+                      // DROPDOWN FILTERS
+                      // Category & Sort Dropdown
+                      // ------------------------------------------------------
+                      Row(
+                        children: [
+                          Flexible(
+                            child: DropdownFilter(
+                              title: 'Category',
+                              fillColor: AppColors.WHITE4,
+                              items: _categories,
+                              selectedCategory: _category,
+                              onItemSelected: (val) {
+                                _category = val;
+                                setState(() {});
+                                context
+                                    .read<ForumBloc>()
+                                    .add(LoadForum(category: val));
+                              },
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    ListView.builder(
-                      itemCount: state.forums.length,
-                      shrinkWrap: true,
-                      padding: EdgeInsets.zero,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        // if (index + 1 == state.forums.length &&
-                        //     !state.hasReachedEnd) {
-                        //   context
-                        //       .read<ForumBloc>()
-                        //       .add(LoadForum(hardRefresh: true));
-                        // }
+                          const SizedBox(width: 10),
+                          Flexible(
+                            child: DropdownFilter(
+                              title: 'Sort',
+                              fillColor: AppColors.WHITE4,
+                              items: ['Sort1', 'Sort2', 'Sort3'],
+                              onItemSelected: (val) {},
+                            ),
+                          ),
+                        ],
+                      ),
+                      // ------------------------------------------------------
+                      // LISTVIEW FOR THE FORUM QUESTIONS
+                      // ------------------------------------------------------
+                      if (_forums != null)
+                        ListView.builder(
+                          itemCount: _forums!.length,
+                          shrinkWrap: true,
+                          padding: EdgeInsets.zero,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            // if (index + 1 == _forums!.length &&
+                            //     !state.hasReachedEnd) {
+                            //   context
+                            //       .read<ForumBloc>()
+                            //       .add(LoadForum(hardRefresh: true));
+                            // }
 
-                        return QuestionItem(
-                          question: state.forums[index],
-                          onTap: () {
-                            context
-                                .read<AnswerBloc>()
-                                .add(LoadAnswers(state.forums[index]));
+                            return QuestionItem(
+                              question: _forums![index],
+                              onTap: () {
+                                context
+                                    .read<AnswerBloc>()
+                                    .add(LoadAnswers(_forums![index]));
 
-                            Navigator.push(
-                                context,
-                                getRoute(ForumDiscussScreen(
-                                  isAuthenticated: widget.isAuthenticated,
-                                )));
+                                Navigator.push(
+                                    context,
+                                    getRoute(ForumDiscussScreen(
+                                      isAuthenticated: widget.isAuthenticated,
+                                    )));
+                              },
+                            );
                           },
-                        );
-                      },
-                    ),
-                  ],
-                )),
-          );
-        }
-
-        return const Center(
-          child: CircularProgressIndicator(),
+                        ),
+                    ],
+                  )),
+            ),
+            // ----------------------------------------------------------------
+            // CIRCULARPROGRESSINDICATOR IS DISPLAYED
+            // WHEN STATE IS FORUMLOADINPROGRESS
+            // ----------------------------------------------------------------
+            if (state is ForumLoadInProgress)
+              const Center(child: CircularProgressIndicator())
+          ],
         );
       }),
+      // ----------------------------------------------------------------------
+      // FAB BUTTON FOR ADDING QUESTION
+      // VISIBIBLE IF _showAddBtn == true
+      // ----------------------------------------------------------------------
       floatingActionButton: Visibility(
         visible: _showAddBtn,
         child: FloatingActionButton(
